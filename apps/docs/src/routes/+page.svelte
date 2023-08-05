80fc2f38f1c8ff96f6e0197ec066a806b8c8e116
RL <richard@kaashin.com>
2023-08-05 00:44:09 -0600

    Add a matterjs canvas for a demo

--- apps/docs/src/routes/+page.svelte
+++ apps/docs/src/routes/+page.svelte
@@ -2,12 +2,52 @@
 	import Unlog from '@polarforge/unlog';
 	import { writable } from 'svelte/store';
 
+	import Matter from 'matter-js';
+	import { onMount } from 'svelte';
+
+	let canvasEl;
+
+	let { Engine, Render, Runner, Bodies, Composite } = Matter;
+
+	onMount(() => {
+		let engine = Engine.create();
+
+		console.log({ canvasEl });
+		let canvasWidth = canvasEl.scrollWidth;
+		let canvasHeight = canvasEl.scrollHeight;
+
+		let render = Render.create({
+			canvas: canvasEl,
+			engine: engine,
+			options: {
+				width: canvasWidth,
+				height: canvasHeight,
+				wireframes: false,
+				pixelRatio: 1
+			}
+		});
+
+		let boxA = Bodies.rectangle(400, 200, 80, 80);
+		let boxB = Bodies.rectangle(450, 50, 80, 80);
+		let ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+
+		Composite.add(engine.world, [boxA, boxB, ground]);
+
+		Render.run(render);
+
+		let runner = Runner.create();
+
+		Runner.run(runner, engine);
+	});
+
 	const TestStore = writable(5);
 </script>
 
 <style>
 </style>
 
-{$TestStore}
+<!-- {$TestStore} -->
+
+<canvas style:width="100vw" style:height="100vh" bind:this={canvasEl} />
 
 <Unlog stores={[{ name: 'Test', store: TestStore }]} enable={import.meta.env.DEV} />
